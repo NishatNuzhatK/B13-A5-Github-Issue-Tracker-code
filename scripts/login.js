e08e541d861ebf043e9712b6a41bb8fb29fc3cbc
NishatNuzhatK <nishat.nuzhat.k@gmail.com>
2026-03-07 12:15:15 +0600

    connected the index page with main page

---
+++ scripts/login.js
@@ -0,0 +1,19 @@
+console.log('log in js connected');
+
+document.getElementById('signin-btn').addEventListener('click', function(){
+
+    const adminName = document.getElementById('input-name').value;
+    const password = document.getElementById('input-pass').value;
+
+    // console.log(adminName, password);
+
+    if(adminName =='admin' && password == 'admin123'){
+        alert('sign in successful');
+        window.location.assign("main.html");
+    }
+
+    else{
+        alert('sign in failed');
+    }
+
+});
